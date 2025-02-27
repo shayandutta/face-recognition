7782d224df8989c141b952cb4dcb205b0fdb599a
Nilav <nilavtalukdar06@gmail.com>
2025-02-27 19:18:30 +0530

    Some minor changes

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const PORT = 5001;
+const PORT = 5001 || process.env.PORT;
 
 app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
-  }
+  },
 });
 
 const upload = multer({ storage });
 
-
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
   if (!req.file) {
-    console.error('No file uploaded.');
-    return res.status(400).json({ error: 'No file uploaded.' });
+    console.error("No file uploaded.");
+    return res.status(400).json({ error: "No file uploaded." });
   }
 
-  console.log('Received file:', req.file);
+  console.log("Received file:", req.file);
 
   const fileUrl = `http://localhost:${PORT}/${req.file.filename}`;
   res.status(200).json({ fileName: req.file.filename, filePath: fileUrl });
 });
 
-const uploadDir = path.join(__dirname, 'uploads');
+const uploadDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
